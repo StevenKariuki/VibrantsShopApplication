92241df1d61b5c2b6ca83cc5f3680f0f442324a6
user <steven.kariuki9@outlook.com>
2023-05-05 10:58:00 +0300

    final1 commit

--- app/src/main/java/com/example/vibrantsshopapplication/MainActivity.kt
+++ app/src/main/java/com/example/vibrantsshopapplication/MainActivity.kt
@@ -4,25 +4,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val signUpButton = findViewById<Button>(R.id.signup_button)
+
+
+//        val signUpButton = findViewById<Button>(R.id.signup_button)
         val loginButton = findViewById<Button>(R.id.login_button)
 
         loginButton.setOnClickListener {
             Toast.makeText(this, "WELCOME BACK", Toast.LENGTH_SHORT).show()
-
+            val loginIntent = Intent(applicationContext, VibrantsPageApplication::class.java)
+            startActivity(loginIntent)
         }
 
-        signUpButton.setOnClickListener {
-            Toast.makeText(this, "Sign Up", Toast.LENGTH_SHORT).show()
-            val signUpIntent = Intent(this, SignupApplication::class.java)
-            startActivity(signUpIntent)
+//        signUpButton.setOnClickListener {
+//            Toast.makeText(this, "Sign Up", Toast.LENGTH_SHORT).show()
+//            val signupIntent = Intent(applicationContext, SignupApplication::class.java)
+//            startActivity(signupIntent)
+//        }
 
+        val signuptext = findViewById<TextView>(R.id.signup_text)
+        signuptext.setOnClickListener {
+            val intent = Intent(applicationContext, SignupApplication::class.java)
+            startActivity(intent)
         }
 
     }
--- app/src/main/java/com/example/vibrantsshopapplication/SignupApplication.kt
+++ app/src/main/java/com/example/vibrantsshopapplication/SignupApplication.kt
@@ -1,11 +1,23 @@
 package com.example.vibrantsshopapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlin.math.sign
 
 class SignupApplication : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_application)
+
+        val signUpButton = findViewById<Button>(R.id.signup_button)
+
+        signUpButton.setOnClickListener {
+            Toast.makeText(this, "WELCOME TO VI_BRANT'S ", Toast.LENGTH_SHORT).show()
+            val signUpIntent = Intent(this, MainActivity::class.java)
+            startActivity(signUpIntent)
+        }
     }
 }
