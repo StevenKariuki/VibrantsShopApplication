8cc5c1c5a71dbc0cb1ed7fce5eccbe74aafaa2ca
user <steven.kariuki9@outlook.com>
2023-05-12 10:22:17 +0300

    settings change

--- app/src/main/java/com/example/vibrantsshopapplication/Settings.kt
+++ app/src/main/java/com/example/vibrantsshopapplication/Settings.kt
@@ -1,12 +1,38 @@
 package com.example.vibrantsshopapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.SeekBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class Settings : AppCompatActivity() {
+class HomeSettingsActivity : AppCompatActivity() {
+
+    private lateinit var notificationCheckBox: CheckBox
+    private lateinit var volumeSeekBar: SeekBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        // Find views by their respective IDs
+        val saveButton: Button = findViewById(R.id.saveButton)
+        notificationCheckBox = findViewById(R.id.notificationCheckBox)
+        volumeSeekBar = findViewById(R.id.volumeSeekBar)
+
+        // Set click listener for the save button
+        saveButton.setOnClickListener {
+            saveSettings()
+        }
+    }
+
+    private fun saveSettings() {
+        val enableNotifications = notificationCheckBox.isChecked
+        val volumeLevel = volumeSeekBar.progress
+
+        // TODO: Save the settings to your app's preferences or database
+
+        Toast.makeText(this, "Settings saved!", Toast.LENGTH_SHORT).show()
     }
 }
